076b829f715bc79ec80181d4ac2604fa38985f58
Gayane Kazhoyan <gaya@cs.uni-bremen.de>
2017-02-24 20:01:21 +0100

    [common] robosherlock LOOK-FOR is now only a motion:
    actions are different from robot to robot

--- cram_sherpa_robots_common/src/robosherlock-action.lisp
+++ cram_sherpa_robots_common/src/robosherlock-action.lisp
@@ -67,42 +67,45 @@
 (defun get-robosherlock-action-client ()
   (or *robosherlock-action-client* (init-robosherlock-action-client)))
 
-(defun call-robosherlock-action (object-name &optional action-timeout)
+(defun call-robosherlock-action (object-name object-cram-name &optional action-timeout)
   (declare (type string object-name)
            (type (or null number) action-timeout))
   (roslisp:ros-info (robots-common robosherlock-client)
                     "Calling CALL-ROBOSHERLOCK-ACTION with object ~a" object-name)
   (unless action-timeout
     (setf action-timeout *robosherlock-action-timeout*))
-  (nth-value
-   1
-   (cpl:with-failure-handling
-       ((simple-error (e)
-          (format t "Actionlib error occured!~%~a~%Reinitializing...~%~%" e)
-          (init-robosherlock-action-client)
-          (cpl:retry)))
-     (let ((actionlib:*action-server-timeout* 10.0))
-       (actionlib:call-goal
-        (get-robosherlock-action-client)
-        (robots-common:make-symbol-type-message
-         'iai_robosherlock_msgs-msg:HighlightObjectGoal
-         :obj_name object-name)
-        :timeout action-timeout)))))
+  (cpl:with-failure-handling
+      ((simple-error (e)
+         (format t "Actionlib error occured!~%~a~%Reinitializing...~%~%" e)
+         (init-robosherlock-action-client)
+         (cpl:retry)))
+    (let ((actionlib:*action-server-timeout* 10.0))
+      (multiple-value-bind (result status)
+          (actionlib:call-goal
+           (get-robosherlock-action-client)
+           (robots-common:make-symbol-type-message
+            'iai_robosherlock_msgs-msg:HighlightObjectGoal
+            :obj_name object-name)
+           :timeout action-timeout)
+        (roslisp:ros-info (robots-common action-client)
+                          "Done with RoboSherlock action with status ~a and result:~%~a"
+                          status result)
+        (let ((object-pose (cl-transforms-stamped:from-msg
+                            (roslisp:msg-slot-value
+                             result
+                             'iai_robosherlock_msgs-msg:pose))))
+          (cram-occasions-events:on-event (make-instance 'robots-common:object-found
+                                          :object object-cram-name :pose object-pose))
+          object-pose)))))
 
 
 
-
-(def-fact-group robosherlock-desigs (desig:motion-desig desig:action-desig)
-  (<- (motion-desig ?motion-designator (look-for ?object-name))
+(def-fact-group robosherlock-desigs (desig:motion-desig)
+  (<- (motion-desig ?motion-designator (look-for ?object-name ?object-cram-name))
     (or (desig-prop ?motion-designator (:type :looking-for))
         (desig-prop ?motion-designator (:to :look-for)))
     (desig-prop ?motion-designator (:object ?object-cram-name))
-    (lisp-fun cram-owl-name ?object-cram-name ?object-name))
-
-  (<- (action-desig ?action-designator (look-for ?object-name))
-    (or (desig-prop ?action-designator (:type :looking-for))
-        (desig-prop ?action-designator (:to :look-for)))
-    (desig-prop ?action-designator (:object ?object-name))))
+    (lisp-fun cram-owl-name ?object-cram-name ?object-name)))
 
 (def-fact-group robosherlock-pm (cpm:matching-process-module cpm:available-process-module)
   (<- (cpm:matching-process-module ?motion-designator robosherlock-pm)
@@ -112,17 +115,14 @@
     (not (cpm:projection-running ?_))))
 
 (cpm:def-process-module robosherlock-pm (motion-designator)
-  (destructuring-bind (command argument) (reference motion-designator)
+  (destructuring-bind (command argument-1 argument-2) (reference motion-designator)
     (ecase command
       (look-for
        (handler-case
-           (call-robosherlock-action argument)
+           (call-robosherlock-action argument-1 argument-2)
          ;; (cram-plan-failures:look-at-failed ()
          ;;   (cpl:fail 'cram-plan-failures:look-at-failed :motion motion-designator))
          )))))
-
-(defun look-for (?object-name)
-  (perform (desig:a motion (to look-for) (object ?object-name))))
 
 ;; example:
 ;; (cpm:with-process-modules-running
